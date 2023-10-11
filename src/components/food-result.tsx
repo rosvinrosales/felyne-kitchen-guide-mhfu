779fe4296e92d48fc467c6b30d8c3f720bd5e2b5
Rosvin <rosvinrosales@gmail.com>
2023-10-11 15:10:08 +0800

    Feat: add checkbox to group food effects

--- src/components/food-result.tsx
+++ src/components/food-result.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { getFoodEffects } from "@/lib/utils";
+import { Checkbox } from "@/components/ui/checkbox";
 
 type FoodResultProps = {
   selectedFoods: string[];
@@ -9,11 +11,51 @@ export const FoodResult = ({
   selectedFoods,
   currentFelyne,
 }: FoodResultProps) => {
-  const foodEffects = getFoodEffects(currentFelyne, selectedFoods);
+  const foodEffectsTemp = getFoodEffects(currentFelyne, selectedFoods);
+  const [foodEffects, setFoodEffects] = React.useState(
+    getFoodEffects(currentFelyne, selectedFoods)
+  );
+  const [isEffectsGrouped, setIsEffectsGrouped] = React.useState<
+    boolean | string
+  >(true);
+
+  const handleCheckGroupEffects = (e: string | boolean) => {
+    setIsEffectsGrouped(e);
+    if (e) {
+      setFoodEffects(uniqueFoodCombos);
+    } else {
+      setFoodEffects(getFoodEffects(currentFelyne, selectedFoods));
+    }
+  };
+
+  const uniqueFoodCombos = foodEffectsTemp.filter((foodCombo, index, self) => {
+    return index === self.findIndex((f) => f.effect === foodCombo.effect);
+  });
+
+  React.useEffect(() => {
+    if (isEffectsGrouped) {
+      setFoodEffects(uniqueFoodCombos);
+    } else {
+      setFoodEffects(getFoodEffects(currentFelyne, selectedFoods));
+    }
+  }, [foodEffectsTemp.length]);
 
   return (
     <div className="rounded-lg border p-2">
       <div className="flex flex-wrap flex-col gap-[8px]">
+        <div className="flex items-center space-x-2">
+          <Checkbox
+            id="group"
+            onCheckedChange={(e) => handleCheckGroupEffects(e)}
+            defaultChecked={true}
+          />
+          <label
+            htmlFor="group"
+            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Group Duplicate Effects
+          </label>
+        </div>
         {foodEffects.length > 0 ? (
           foodEffects.map(({ foodCombo, effect, effectStatus }) => (
             <div
