0d0143cf4197f558330a675a9f7468a7e1e3c0bd
Rosvin <rosvinrosales@gmail.com>
2023-10-17 14:20:21 +0800

    Update: remove icons from selected foods

--- src/components/food-selected.tsx
+++ src/components/food-selected.tsx
@@ -1,15 +1,18 @@
 import {
-  Beef,
-  Cherry,
-  Fish,
-  GlassWater,
-  LeafyGreen,
-  Milk,
-  Wheat,
+  // Beef,
+  // Cherry,
+  // Fish,
+  // GlassWater,
+  // LeafyGreen,
+  // Milk,
+  // Wheat,
   X,
 } from "lucide-react";
+
+// import * as LucideIcons from "lucide-react";
+
 import { CardHeader } from "@/components/ui/card";
-import { getFoodType } from "@/lib/utils";
+// import { getFoodType } from "@/lib/utils";
 
 type FoodSelectedProps = {
   selectedFoods: string[];
@@ -20,25 +23,29 @@ export const FoodSelected = ({
   selectedFoods,
   handleRemoveSelectedFood,
 }: FoodSelectedProps) => {
-  const getIcon = (category: string | null) => {
-    switch (category) {
-      case "Bran":
-        return <Wheat />;
-      case "Meat":
-        return <Beef />;
-      case "Fruit":
-        return <Cherry />;
-      case "Dairy":
-        return <Milk />;
-      case "Drink":
-        return <GlassWater />;
-      case "Fish":
-        return <Fish />;
-      case "Veg":
-        return <LeafyGreen />;
-      default:
-    }
-  };
+  // const IconComponent = LucideIcons[getFoodType(food)];
+
+  // const getIcon = (category: string | null) => {
+  //   const baseClass = "mr-2 h-4 w-4";
+
+  //   switch (category) {
+  //     case "Bran":
+  //       return <Wheat className={baseClass} />;
+  //     case "Meat":
+  //       return <Beef className={baseClass} />;
+  //     case "Fruit":
+  //       return <Cherry className={baseClass} />;
+  //     case "Dairy":
+  //       return <Milk className={baseClass} />;
+  //     case "Drink":
+  //       return <GlassWater className={baseClass} />;
+  //     case "Fish":
+  //       return <Fish className={baseClass} />;
+  //     case "Veg":
+  //       return <LeafyGreen className={baseClass} />;
+  //     default:
+  //   }
+  // };
 
   return (
     <div className="rounded-lg border p-2">
@@ -46,28 +53,23 @@ export const FoodSelected = ({
         {selectedFoods.length > 0 ? (
           selectedFoods.map((food, index) => (
             <div
-              className="transition rounded-lg border hover:shadow-md hover:cursor-pointer dark:hover:shadow-gray-800"
+              className="transition rounded-lg border hover:shadow-md hover:cursor-pointer dark:hover:shadow-gray-800 min-w-[90px]"
               key={index}
             >
-              <CardHeader className="px-[15px] py-2.5 capitalize items-center gap-1">
-                <div className="flex justify-end w-full">
-                  <X
-                    className="w-4 h-4 text-gray-400 hover:text-gray-50"
-                    onClick={() => handleRemoveSelectedFood(food)}
-                  />
-                </div>
-                {getIcon(getFoodType(food))}
-                <span className="text-xs text-center">
-                  {food}
-                  <span className="block text-center">
-                    ({getFoodType(food)})
-                  </span>
-                </span>
+              <CardHeader className="px-[15px] py-2.5 capitalize flex-row items-center">
+                {/* {LucideIcons[getFoodType(food)]} */}
+                {/* <IconComponent className="mr-2 h-4 w-4" /> */}
+                {/* {getIcon(getFoodType(food))} */}
+                <span className="text-sm text-center !mt-0">{food}</span>
+                <X
+                  className="w-4 h-4 text-gray-400 hover:text-gray-50 !mt-0 ml-4"
+                  onClick={() => handleRemoveSelectedFood(food)}
+                />
               </CardHeader>
             </div>
           ))
         ) : (
-          <CardHeader className="px-[15px] py-2.5">Empty</CardHeader>
+          <CardHeader className="px-[15px] py-2.5 text-sm">Empty</CardHeader>
         )}
       </div>
     </div>
